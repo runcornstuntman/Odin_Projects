a5649ed5251b1c9400bb914b536a656245f49f5d
Runcornstuntman <136044024+runcornstuntman@users.noreply.github.com>
2024-11-28 23:43:09 +1100

    Add notes

--- 03-scissors-paper-rock/script.js
+++ 03-scissors-paper-rock/script.js
@@ -1,1 +1,5 @@
 console.log("Hello World")
+
+// Train of thought. Have a get user prompt which will link the users choice
+// back to them. Have a random choice for the computer which is selected. 
+// Count down and then display the choices
