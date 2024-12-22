967799c9c34583d64a02c31838afa6eb33687b02
Runcornstuntman <136044024+runcornstuntman@users.noreply.github.com>
2024-12-22 21:40:23 +1000

    Rough idea forming

--- 02-fullstack_javascript/03-javascript/01-Library/script.js
+++ 02-fullstack_javascript/03-javascript/01-Library/script.js
@@ -1,5 +1,7 @@
 
+// Declare my library and ensure counter starts at one.
 const myLibrary = []
+let nextId = 1;
 
 function Book(id, title, author, page_numbers, read_or_not) {
     this.id = id;
@@ -13,29 +15,12 @@ function Book(id, title, author, page_numbers, read_or_not) {
     }
 }
 
-function addBookToLibrary() {
-    // Do things here
+function addBookToLibrary(title, author, page_numbers, read_or_not) {
+    // The current value of nextId is 1 and this is used before incrementing
+    // Think of it as Book() === nextId => nextId++ === 2 => Book() === nextID(now 2)
+    const newBook = new Book(nextId++, title, author, page_numbers, read_or_not);
+    myLibrary.push(newBook);
 }
-
-/*
-All of your book objects are going to be stored in an array, so add a function to the script (not the constructor) that can take some arguments, 
-create a book from those arguments, and store the new book object into an array. Your code should look something like above.
-
-Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on 
-their own “card”. It might help for now to manually add a few books to your array so you can see the display.
-
-
-
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not yet read')
-
-
-
-theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-// If not changed to return, and left at console.log() in function will not return correctly
-console.log(theHobbit.info());
-
-
-*/
 
 
 
